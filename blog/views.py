4ce942b299cdca1001b8f34e925dfc71ff8a8e48
Kael Spencer <kaelspencer@gmail.com>
2012-05-13 16:29:10 -0700

    Create blog archive overview
    Create a page for /blog/ that is different than the rest. It gives the first few
    words of each article. This page is a one-column layout.

--- blog/views.py
+++ blog/views.py
@@ -10,18 +10,21 @@ class EntryView(View):
     
     def get_queryset(self):
         return Entry.objects.all() if self.request.user.is_staff else Entry.objects.published()
-    
+
+class EntryFrontPage(EntryView, dates.ArchiveIndexView):
+    template_name = 'front_page.html'
+
 class EntryArchiveIndex(EntryView, dates.ArchiveIndexView):
-    template_name = 'archive.html'
+    template_name = 'archive_overview.html'
     
 class EntryArchiveYear(EntryView, dates.YearArchiveView):
     template_name = 'archive_year.html'
     
 class EntryArchiveMonth(EntryView, dates.MonthArchiveView):
-    template_name = 'archive.html'
+    template_name = 'front_page.html'
     
 class EntryArchiveDay(EntryView, dates.DayArchiveView):
-    template_name = 'archive.html'
+    template_name = 'front_page.html'
     
 class EntryArchiveDetail(EntryView, dates.DateDetailView):
     template_name = 'detail.html'    
--- urls.py
+++ urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
-from .blog.views import EntryArchiveIndex
+from .blog.views import EntryFrontPage
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', EntryArchiveIndex.as_view(), name='www_url_index'),
+    url(r'^$', EntryFrontPage.as_view(), name='www_url_index'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='www_url_about'),
     url(r'^admin/', include('smuggler.urls')),
     url(r'^admin/', include(admin.site.urls)),
