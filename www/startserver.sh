b5f98f3536995f0f66805eb06606581a0d4efcf8
Kael Spencer <kael@euler>
2012-08-19 12:29:42 -0700

    Ignore virtualenv and Add Start Script Ignore the newly created virtualenv folder and add the startserver.sh script.

---
+++ www/startserver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Replace these three settings.
+PROJDIR="/home/kael/django/www"
+PIDFILE="$PROJDIR/www.pid"
+
+cd $PROJDIR
+if [ -f $PIDFILE ]; then
+    kill `cat -- $PIDFILE`
+    rm -f -- $PIDFILE
+fi
+
+exec /usr/bin/env - \
+  PYTHONPATH="../python:.." \
+  ./manage.py runfcgi protocol=fcgi host=127.0.0.1 port=8000 pidfile=$PIDFILE
