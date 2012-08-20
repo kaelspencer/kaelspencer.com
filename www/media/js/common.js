615ac05430204565c16ebf7631d7206f98b6f1aa
Kael Spencer <kael@euler>
2012-08-19 23:00:59 -0700

    Show Git Repos A list of my Github repositories is displayed underneath the link in the "Me" sidebar. This is retrieved via the Github APIs after the page finishes loading.

--- www/media/js/common.js
+++ www/media/js/common.js
@@ -3,4 +3,23 @@ $(function(){
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
+
+    $.ajax({
+        type: "GET",
+        url: "https://api.github.com/users/kaelspencer/repos",
+        datatype: "jsonp",
+        error: function(data, textstatus, errorthrown) {
+            console.log('Error: ' + textstatus);
+        },
+        success: function(data) {
+            var list = $('#gitlist')
+
+            $.each(data, function(index, repo) {
+                list = $('<li>')
+                    .insertAfter(list)
+                    .addClass('sublist')
+                    .html('<a href="' + repo.html_url + '">' + repo.name + '</a>');
+            });
+        }
+    });
 });
