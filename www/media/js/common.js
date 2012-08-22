ae6ffc972ecbc41e28fb387632835d74f03669e1
Kael Spencer <kael@euler>
2012-08-21 23:08:04 -0700

    Fix Cross Domain Issue The cross domain ajax request worked locally, but not on the production server. Instead of ajax() being called, getJSON is used. This handles the cross domain issue internally.

--- www/media/js/common.js
+++ www/media/js/common.js
@@ -4,22 +4,14 @@ $(function(){
         return false;
     });
 
-    $.ajax({
-        type: "GET",
-        url: "https://api.github.com/users/kaelspencer/repos",
-        datatype: "jsonp",
-        error: function(data, textstatus, errorthrown) {
-            console.log('Error: ' + textstatus);
-        },
-        success: function(data) {
-            var list = $('#gitlist')
+    $.getJSON('https://api.github.com/users/kaelspencer/repos?callback=?', function(data) {
+        var list = $('#gitlist');
 
-            $.each(data, function(index, repo) {
-                list = $('<li>')
-                    .insertAfter(list)
-                    .addClass('sublist')
-                    .html('<a href="' + repo.html_url + '">' + repo.name + '</a>');
-            });
-        }
+        $.each(data.data, function(index, repo) {
+            list = $('<li/>', {
+                'class': 'sublist',
+                html: '<a href="' + repo.html_url + '">' + repo.name + '</a>'
+            }).insertAfter(list);
+        });
     });
 });
