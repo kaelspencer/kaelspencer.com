d7e1c4cb4ab53dbe4ab382c6e46f7a80d5cb46ab
kaelspencer <kael@descartes>
2013-04-28 19:37:11 -0700

    Style Table and Parse System Name This change adds styling for the table where the items are outputted. Because the system name isn't provide it is parsed out from the station name. This might not always be accurate.

--- www/media/js/eve/shopper/shopper.js
+++ www/media/js/eve/shopper/shopper.js
@@ -4,6 +4,8 @@ var EveShopper = (function() {
         this.m_api = 'http://api.eve-central.com/api/';
         this.m_apiRoute = this.m_api + 'route/';
         this.m_apiQuicklook = this.m_api + 'quicklook';
+        this.m_sellOrders = undefined;
+        this.m_jumpCount = Array();
     };
 
     EveShopper.prototype.runTest = function() {
@@ -11,7 +13,33 @@ var EveShopper = (function() {
             //url: this.m_apiQuicklook + '?typeid=4305',
             url: '/media/js/eve/shopper/quicklook.xml',
             dataType: 'xml',
+            context: this,
             success: this.onQuicklook
+        }).done(function() {
+            console.log(this.m_sellOrders);
+
+            if (this.m_sellOrders) {
+                var container = $('#sell_orders tbody');
+                var odd = false;
+
+                this.updateJumpCounts(this.m_sellOrders);
+
+                $.each(this.m_sellOrders, function() {
+                    var tr = $('<tr />')
+                        .append($('<td />', { text: this.station_name }))
+                        .append($('<td />', { text: '0' }))
+                        .append($('<td />', { text: this.price }))
+                        .append($('<td />', { text: this.system }));
+
+                    if (odd) {
+                        tr.addClass('odd');
+                    }
+
+                    odd = !odd;
+
+                    container.append(tr);
+                });
+            }
         });
     };
 
@@ -19,9 +47,9 @@ var EveShopper = (function() {
         var xml = $(data).children('evec_api').children('quicklook');
         var sell = xml.children('sell_orders').children('order');
         var buy = xml.children('buy_orders').children('order');
-        var container = $('#sell_orders');
-
         var sellOrders = Array();
+
+        $('#sell_header').text('Sell Orders: ' + xml.children('itemname').text());
 
         sell.each(function() {
             sellOrders.push({
@@ -35,14 +63,19 @@ var EveShopper = (function() {
             return (a.price == b.price ? 0 : (a.price < b.price ? -1 : 1));
         });
 
-        $.each(sellOrders, function() {
-            var tr = $('<tr />');
-            tr.append($('<td />', { text: this.station_name }));
-            tr.append($('<td />', { text: '0' }));
-            tr.append($('<td />', { text: this.price }));
+        this.m_sellOrders = sellOrders;
+    };
 
-            container.append(tr);
+    EveShopper.prototype.updateJumpCounts = function(orders) {
+        $.each(orders, function(key, value) {
+            var split = value.station_name.split(' - ');
+            var stationArr = split[0].split(' ');
+            var system = stationArr.splice(0, stationArr.length - 1).join(' ');
+
+            orders[key].system = system;
+            this.m_jumpCount[system] = 0;
         });
+
     };
 
     return EveShopper;
