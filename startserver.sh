b9e110998ec4c9dc13a7ef065c30414c029b6a49
Kael Spencer <kaelspencer@gmail.com>
2014-09-27 23:26:10 +0000

    Update Startup Script for Supervisord

--- startserver.sh
+++ startserver.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Replace these three settings.
-PROJDIR=$PWD
-PIDFILE="$PROJDIR/www.pid"
-
-cd $PROJDIR
-if [ -f $PIDFILE ]; then
-    kill `cat -- $PIDFILE`
-    rm -f -- $PIDFILE
-fi
-
 . ../virtualenv/kaelspencer.com/bin/activate
 
-exec python manage.py runfcgi protocol=fcgi host=127.0.0.1 port=8000 pidfile=$PIDFILE
+exec python manage.py runfcgi protocol=fcgi host=127.0.0.1 port=8000 daemonize=false
